c408dcf3f3cd903aa98076bd5a7c3d433a40c2c7
Jonathan <cout.campos@gmail.com>
2023-10-18 15:18:47 -0300

    Alteração do nome da pasta em templates.

--- AluraSpace/galeria/views.py
+++ AluraSpace/galeria/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return render(request, 'template1/index.html')
+    return render(request, 'galeria/index.html')
 
 def imagem(request):
-    return render(request, 'template1/imagem.html')
+    return render(request, 'galeria/imagem.html')
